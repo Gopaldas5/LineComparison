6cb571ad212eac983996579b248da859389e3048
GopalDas <gopalkrdas1997@gmail.com>
2022-06-25 20:47:50 +0530

    ADDED:Checking two lines equality

--- src/com/bridgeLabz/LineComparison.java
+++ src/com/bridgeLabz/LineComparison.java
@@ -5,32 +5,44 @@ import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome Line comparison computation program on master branch ");
-            int x2;
-            int y1;
-            int y2;
-            double dist;
-            Scanner scanner=new Scanner(System.in);
 
-            System.out.println("Enter x1 point:- ");
+        int x1,x2,x3,x4;
+        int y1,y2,y3,y4;
+        double length1;
+        double length2;
+        Scanner scanner=new Scanner(System.in);
 
-            int x1 = scanner.nextInt();
+        System.out.println("Enter x1 point:- ");
+        x1 = scanner.nextInt();
+        System.out.println("Enter y1 point:- ");
+        y1 = scanner.nextInt();
+        System.out.println("Enter x2point:- ");
+        x2 = scanner.nextInt();
+        System.out.println("Enter y2 point");
+        y2 = scanner.nextInt();
 
-            System.out.println("Enter y1 point:- ");
+        length1 = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+        System.out.println("Distance between two point in line1 is:-  "+"("+x1+","+y1+"),"+"("+x2+","+y2+"):- "+length1);
+        Double line1 = Double.valueOf(length1);
 
-            y1=scanner.nextInt();
+        System.out.println("Enter x3 point:- ");
+        x3 = scanner.nextInt();
+        System.out.println("Enter y3 point:- ");
+        y3 = scanner.nextInt();
+        System.out.println("Enter x4 point:- ");
+        x4 = scanner.nextInt();
+        System.out.println("Enter y4 point");
+        y4 = scanner.nextInt();
 
-            System.out.println("Enter x2point:- ");
+        length2 = Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
+        System.out.println("Distance between two point in line2 is:-  "+"("+x3+","+y3+"),"+"("+x4+","+y4+"):- "+length2);
+        Double line2 = Double.valueOf(length2);
 
-            x2=scanner.nextInt();
-
-            System.out.println("Enter y2 point");
-
-            y2=scanner.nextInt();
-
-            dist=Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
-
-            System.out.println("Distance between two point is:-  "+"("+x1+","+y1+"),"+"("+x2+","+y2+"):- "+dist);
-
+        if(line1.equals(line2)){
+            System.out.println("Line1 and line2 are equal");
+        }else{
+            System.out.println("Line1 and line2 are not equal");
+        }
 
         }
 }
