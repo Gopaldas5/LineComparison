4301dad90ee2d4ac161db0d438d50f84d83efd08
GopalDas <gopalkrdas1997@gmail.com>
2022-06-25 20:27:09 +0530

    ADDED:Welcome to Line comparison program

---
+++ src/com/bridgeLabz/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome Line comparison computation program on master branch ");
+    }
+}
